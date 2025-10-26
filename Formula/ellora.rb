5c1576248223a9bc932d042dd8355ec70231da25
Karolis Gudiškis <karolisgud@gmail.com>
2025-10-26 16:20:58 +0200

    chore: Update version to 0.4.0-prerelease-7

--- Formula/ellora.rb
+++ Formula/ellora.rb
@@ -5,11 +5,11 @@ class Ellora < Formula
 
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-5/ai-gateway-macos-aarch64"
-        sha256 "b77713bfc7780ab631e6a266eb31566ebde54a41da74d05f80fa7036da8ea153"  # Run: shasum -a 256 ai-gateway-aarch64  
+        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-7/ai-gateway-macos-aarch64"
+        sha256 "4732eec74d2511f554069240f836c3bd3fe12a6ae7c229c5c162f679f95a34e6"  # Run: shasum -a 256 ai-gateway-aarch64  
       else
-        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-5/ai-gateway-macos-x86_64"
-        sha256 "4d54c68a40baaed38d05d6024c08575564a45d57ba89bd6aef5e6d91efab28c2"  # Run: shasum -a 256 ai-gateway-x86_64
+        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-7/ai-gateway-macos-x86_64"
+        sha256 "5708f242fbb06bd19524f5bb0c4e9a3089e61eea636549d435e173d068c9a231"  # Run: shasum -a 256 ai-gateway-x86_64
       end
     end
 
@@ -18,8 +18,8 @@ class Ellora < Formula
     #     url "https://github.com/langdb/ai-gateway/releases/download/v0.3.2/ai-gateway-linux-aarch64"
     #     sha256 "f9dbe7dfbe1f7a6a817f0d3a674d54ad07062496e5753106d42d916ef450b7b2"  # Run: shasum -a 256 ai-gateway-aarch64
     #   else
-    url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-5/ai-gateway-linux-x86_64"
-    sha256 "7471db2ebf26d8ece206123e0e472e5964300df98b3483c5e8e73add0af9e755"  # Run: shasum -a 256 ai-gateway-x86_64
+    url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-7/ai-gateway-linux-x86_64"
+    sha256 "f0946cc903e840b5b306ccd1788ae516f8f00870bc6ef5246fb027c449b9c227"  # Run: shasum -a 256 ai-gateway-x86_64
     #   end
     end
 
