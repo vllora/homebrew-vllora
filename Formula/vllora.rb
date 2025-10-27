ee14e084c4711780a77acbadaececa7cce7e0530
Karolis Gudiškis <karolisgud@gmail.com>
2025-10-27 08:51:43 +0200

    chore: Update version to 0.4.0-prerelease-11 and corresponding URLs and SHA256 checksums

--- Formula/vllora.rb
+++ Formula/vllora.rb
@@ -1,14 +1,14 @@
-class Ellora < Formula
-    desc "vllora - Multi-provider AI gateway server"
+class Vllora < Formula
+  desc "vllora - Multi-provider AI gateway server"
     homepage "https://github.com/langdb/vllora"
-    version "0.4.0-prerelease-10"  # Update this with your version
+    version "0.4.0-prerelease-11"  # Update this with your version
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-10/ai-gateway-macos-aarch64"
-        sha256 "5e6ed0da4e008d950d5fc66dc12fe5f1d42165fc9b408cefa7cd4964e77de5dc"  # Run: shasum -a 256 ai-gateway-aarch64
+        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-11/ai-gateway-macos-aarch64"
+        sha256 "b655de22488f98ec8ec633911ed86a93a0c8a4e7395fa0caa8f86683ab87c6a7"  # Run: shasum -a 256 ai-gateway-aarch64
       else
-        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-10/ai-gateway-macos-x86_64"
-        sha256 "f31497f451e2546e58aa1e6f58ea3abcb22e57fe27014876a10c1cc29ae160a3"  # Run: shasum -a 256 ai-gateway-x86_64
+        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-11/ai-gateway-macos-x86_64"
+        sha256 "63d79680f3ad5767ae672843a3af4ed58854fb158329356f5eb0fedcb51451c7"  # Run: shasum -a 256 ai-gateway-x86_64
       end
     end
     on_linux do
@@ -16,8 +16,8 @@ class Ellora < Formula
     #     url "https://github.com/langdb/ai-gateway/releases/download/v0.3.2/ai-gateway-linux-aarch64"
     #     sha256 "f9dbe7dfbe1f7a6a817f0d3a674d54ad07062496e5753106d42d916ef450b7b2"  # Run: shasum -a 256 ai-gateway-aarch64
     #   else
-        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-10/ai-gateway-linux-x86_64"
-        sha256 "63c4bc1600ed1c3fdb600d82cb64e481a9b8d2832aa33f1420902b91fcee790a"  # Run: shasum -a 256 ai-gateway-x86_64
+        url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-11/ai-gateway-linux-x86_64"
+        sha256 "53bf1449fba0b371e9a636bd6af92a85b448252c993eb7e69983deab149e4440"  # Run: shasum -a 256 ai-gateway-x86_64
     #   end
     end
     def install
