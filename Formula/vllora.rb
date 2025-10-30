0ba95556363b9f3e53e76f4a5b1561ec9fbf27e1
Karolis Gudiškis <karolisgud@gmail.com>
2025-10-30 06:51:46 +0200

    chore: Update version to 0.4.0-prerelease-30 and corresponding URLs and SHA256 checksums for macOS and Linux

--- Formula/vllora.rb
+++ Formula/vllora.rb
@@ -1,23 +1,23 @@
 class Vllora < Formula
   desc "vllora - Multi-provider AI gateway server"
   homepage "https://github.com/vllora/vllora"
-  version "0.4.0-prerelease-26"  # Update this with your version
+  version ".4.0-prerelease-30"  # Update this with your version
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/vllora/vllora/releases/download/v0.4.0-prerelease-26/ai-gateway-macos-aarch64"
-      sha256 "732e0e5e01103d3fd16fa1d124735c66319d8771b0f65c9f4f3878cb9b7e6293"  # Run: shasum -a 256 ai-gateway-aarch64
+      url "https://github.com/vllora/vllora/releases/download/v.4.0-prerelease-30/ai-gateway-macos-aarch64"
+      sha256 "ecfdca5b04e13f3c3caad8d9c7240c554b35d993ff8d4af08020fdf9a406e297"  # Run: shasum -a 256 ai-gateway-aarch64
     else
-      url "https://github.com/vllora/vllora/releases/download/v0.4.0-prerelease-26/ai-gateway-macos-x86_64"
-      sha256 "05b72e366b307e48e5901d715ef4e2b017ecee4da0b8c781462f46cfbeb1ab7a"  # Run: shasum -a 256 ai-gateway-x86_64
+      url "https://github.com/vllora/vllora/releases/download/v.4.0-prerelease-30/ai-gateway-macos-x86_64"
+      sha256 "751028385c02fec818d71c80da699cf2863de8698b7900c3358cdf3443d4cf64"  # Run: shasum -a 256 ai-gateway-x86_64
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/vllora/vllora/releases/download/v0.4.0-prerelease-26/ai-gateway-linux-aarch64"
-      sha256 "49aef94a80b0dc08eafcb7ccc554d09b27c1b7ff537fcb97d9ec25bf969d36ca"  # Will be updated by CI
+      url "https://github.com/vllora/vllora/releases/download/v.4.0-prerelease-30/ai-gateway-linux-aarch64"
+      sha256 "4117958557d9a5254cda5648402b649f5f08cf1af5cc6e920874d3bc1e68d617"  # Will be updated by CI
     else
-      url "https://github.com/vllora/vllora/releases/download/v0.4.0-prerelease-26/ai-gateway-linux-x86_64"
-      sha256 "f7247c7e80c56b73a7bdf9ac9987b1cd57bff7c9107cb437211d358d108046e5"  # Run: shasum -a 256 ai-gateway-x86_64
+      url "https://github.com/vllora/vllora/releases/download/v.4.0-prerelease-30/ai-gateway-linux-x86_64"
+      sha256 "34bf406e8e8737c53b658b01b699f3b9d9a2a548132dbf98e1fa06920932b9f9"  # Run: shasum -a 256 ai-gateway-x86_64
     end
   end
   def install
