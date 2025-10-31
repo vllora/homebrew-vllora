9835fba157ff73b8f9fab3cb3d9b7dc68ca2881f
Karolis Gudiškis <karolisgud@gmail.com>
2025-10-31 07:08:42 +0200

    vllora 0.4.0-prerelease-36: update URLs in formula and workflow for macOS and Linux binaries

--- Formula/vllora.rb
+++ Formula/vllora.rb
@@ -4,19 +4,19 @@ class Vllora < Formula
   version "0.4.0-prerelease-36"  # Update this with your version
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/vllora/vllora/vllora-macos-aarch64"
+      url "https://github.com/vllora/vllora/releases/download/v0.4.0-prerelease-36/vllora-macos-aarch64"
       sha256 "41b026a906f412eb4d571f858c499d5afe4ac49b90a67cc13aa9cc71f1154556"  # Run: shasum -a 256 ai-gateway-aarch64
     else
-      url "https://github.com/vllora/vllora/vllora-macos-x86_64"
+      url "https://github.com/vllora/vllora/releases/download/v0.4.0-prerelease-36/vllora-macos-x86_64"
       sha256 "95dd6a415b28e6e7f42732efc5df81d4b67d496a0f26cf7f833b7b2ceafa55bf"  # Run: shasum -a 256 ai-gateway-x86_64
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/vllora/vllora/vllora-linux-aarch64"
+      url "https://github.com/vllora/vllora/releases/download/v0.4.0-prerelease-36/vllora-linux-aarch64"
       sha256 "8774e23ff8087f1d46e6ef8d8fce1343a40103480cd955ca1601be3909e92e1f"  # Will be updated by CI
     else
-      url "https://github.com/vllora/vllora/vllora-linux-x86_64"
+      url "https://github.com/vllora/vllora/releases/download/v0.4.0-prerelease-36/vllora-linux-x86_64"
       sha256 "06327d723d565a91d4607fe46b0f75c300b33ab76c370cb1ff126886306fff3f"  # Run: shasum -a 256 ai-gateway-x86_64
     end
   end
